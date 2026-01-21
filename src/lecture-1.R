f148935d98bcaddcf14afdc9e55862219d115d4d
Romain Ferrali <rferrali@gmail.com>
2026-01-21 08:21:52 +0000

    sketch lecture 1

---
+++ src/lecture-1.R
@@ -0,0 +1,19 @@
+##################################
+## Lecture 1: Introduction to R ##
+##################################
+
+## Preliminaries: the console and script files ---------------
+
+## R as a calculator -----------------------------------------
+
+## Creating, overwriting and deleting objects ----------------
+
+## Data types ------------------------------------------------
+
+# vectors (and their types)
+
+# matrices
+
+# lists
+
+# data frames
