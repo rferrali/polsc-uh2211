15dbea738e2088b0aece76b9cfdd10643cac736f
Romain Ferrali <rferrali@gmail.com>
2026-01-25 16:32:20 +0000

    sketch lecture 2

---
+++ src/lecture-2.R
@@ -0,0 +1,29 @@
+#######################################
+## Lecture 2: Object types, datasets ##
+#######################################
+
+## Object types -------------------------------
+
+# numeric
+# character
+# logical
+
+# vectors: seq(), c(), rep()
+
+# matrices (coercion, rbind(), cbind())
+
+# lists
+
+# knowing the type of an object: class()
+
+# an important, more elaborate object: data.frame
+
+## Datasets -------------------------------
+
+# reading data
+
+# exploring data: head(), summary(), table(), unique()
+
+# accessing data: $, subsetting
+
+# counting things: length(), nrow(), ncol(), dim()
